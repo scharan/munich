a75c329aa437a34b80c9ea3a61c40b01fce9dadd
Sai Charan <me@saicharan.in>
2011-08-17 12:12:58 -0700

    Prototype adapter.

---
+++ prettify-adapter.js
@@ -0,0 +1,15 @@
+// Author: Sai Charan K.
+// Distributed without warranties or conditions of any kind, either
+// express or implied except those under the CC BY-NC 3.0 license.
+// You may obtain a copy of the license at:
+//
+// http://creativecommons.org/licenses/by-nc/3.0/
+
+// Depends on jQuery (currently tested with version 1.6.2)
+// 
+// Aims to:
+// 1. Transform <pre class="brush: cpp"> into <pre class="prettyprint lang-cpp">
+
+$(document).ready(function() {
+    alert( $("pre") );
+});
